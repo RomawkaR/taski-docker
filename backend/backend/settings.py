ae75401fa2618ede54860b984085f14137e68bf5
Роман Родионов <114012056+RomawkaR@users.noreply.github.com>
2023-07-12 18:26:45 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -63,8 +63,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'taski'),
-        'USER': os.getenv('POSTGRES_USER', 'taski'),
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
